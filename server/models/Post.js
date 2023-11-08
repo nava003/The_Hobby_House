2d79cd53189eb98faf097afba40f39bfb93a2c0c
MarkoAndreev <68433525+MarkoAndreev@users.noreply.github.com>
2023-11-07 19:57:04 -0600

    added typeDefs and some of the resolvers

---
+++ server/models/Post.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const postSchema = new Schema({
+//   postImage: {
+//     type: String,
+//     required: true
+//   },
+  postCaption: {
+    type: String,
+    maxlength: 150,
+    trim: true,
+  },
+  postDesc: {
+    type: String,
+    required: "Leave a description!",
+    minlength: 10,
+    maxlength: 300,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  comments: [
+    {
+      commentText: {
+        type: String,
+      },
+      commentAuthor: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    },
+  ],
+});
+
+const Post = model("Post", postSchema);
+
+module.exports = Post;
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,101 @@
+const { User, Post } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find().populate("posts");
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate("posts");
+    },
+    posts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Post.find(params).sort({ createdAt: -1 });
+    },
+    post: async (parent, { postId }) => {
+      return Post.findOne({ _id: postId });
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate("posts");
+      }
+      throw AuthenticationError;
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addPost: async (parent, { postDesc }, context) => {
+      if (context.user) {
+        const post = await Post.create({
+          postDesc,
+          postAuthor: context.user.username,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { posts: post._id } }
+        );
+
+        return post;
+      }
+      throw AuthenticationError;
+    },
+    addComment: async (parent, { postId, commentText }, context) => {
+      if (context.user) {
+        return Post.findOneAndUpdate(
+          { _id: postId },
+          {
+            $addToSet: {
+              comments: { commentText, commentAuthor: context.user.username },
+            },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      throw AuthenticationError
+    },
+    removePost: async (parent, {postId}, context) => {
+        if (context.user){
+            const post = await Post.findOneAndDelete({
+                _id: postId,
+                postAuthor: context.user.username
+            })
+
+            await User.findOneAndUpdate(
+                {
+                    _id: context.user._id
+                },
+                {
+                    $pull: {posts: post._id}
+                }
+                )
+        }
+    }
+  },
+};
