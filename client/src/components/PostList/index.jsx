0ba54f6facaadcb3db51a8575c376e1e55407080
BabyStu <srw33de@gmail.com>
2023-11-08 17:50:21 -0600

    added login and signup jsx

---
+++ client/src/components/PostList/index.jsx
@@ -0,0 +1,53 @@
+import { Link } from 'react-router-dom';
+
+const PostList = ({
+  posts,
+  title,
+  showTitle = true,
+  showUsername = true,
+}) => {
+  if (!posts.length) {
+    return <h3>No Posts Yet</h3>;
+  }
+
+  return (
+    <div>
+      {showTitle && <h3>{title}</h3>}
+      {Posts &&
+        Posts.map((Post) => (
+          <div key={Post._id} className="">
+            <h4 className="">
+              {showUsername ? (
+                <Link
+                  className="text-light"
+                  to={`/profiles/${Post.PostAuthor}`}
+                >
+                  {Post.PostAuthor} <br />
+                  <span style={{ fontSize: '1rem' }}>
+                    had this Post on {Post.createdAt}
+                  </span>
+                </Link>
+              ) : (
+                <>
+                  <span style={{ fontSize: '1rem' }}>
+                    You had this Post on {Post.createdAt}
+                  </span>
+                </>
+              )}
+            </h4>
+            <div className="">
+              <p>{Post.PostText}</p>
+            </div>
+            <Link
+              className=""
+              to={`/Posts/${Post._id}`}
+            >
+              Join the discussion on this Post.
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default PostList;
