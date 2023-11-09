2d9b5221d5a9f2c9332995b7440ea196efd207c1
NavaHub <navaa003@gmail.com>
2023-11-08 21:00:53 -0600

    Fixed issues regarding packages, client-side exports, and added some structure to some jsx files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,2 +1,16 @@
 import { useQuery } from '@apollo/client';
-//import
+import { PostList } from '../components/PostList/index.jsx';
+import { QUERY_POSTS } from '../utils/queries';
+
+const Home = () => {
+    const { loading, data } = useQuery(QUERY_POSTS);
+    const posts = data?.posts || [];
+
+    return (
+        <div className='container'>
+            <PostList />
+        </div>
+    )
+}
+
+export default Home;
