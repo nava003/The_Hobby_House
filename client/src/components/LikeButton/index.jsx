afea9afa87e1fdb72b85dead5928ebc343f25cb2
MarkoAndreev <68433525+MarkoAndreev@users.noreply.github.com>
2023-11-14 18:34:19 -0600

    added css to like button

--- client/src/components/LikeButton/index.jsx
+++ client/src/components/LikeButton/index.jsx
@@ -1,28 +1,47 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { useMutation } from '@apollo/client';
-import { ADD_LIKE } from '../../utils/mutations';
+import { useMutation } from "@apollo/client";
+import { ADD_LIKE } from "../../utils/mutations";
 
+const LikeButton = ({ postId, initialLikes }) => {
+  const likesCount = initialLikes || 0;
 
+  const [likes, setLikes] = useState(likesCount);
+  const [liked, setLiked] = useState(false);
+  const [overLike, setOverLike] = useState(false);
+
+  const [addLike, { error, data }] = useMutation(ADD_LIKE);
 const LikeButton = ({ postId }) => {
     const [likes, setLikes] = useState(0);
     const [addLike, { error, data }] = useMutation(ADD_LIKE);
 
-    const likePost = async () => {
-        try {
-            const { data } = await addLike({ variables: { postId } });
-            setLikes(likes + 1);
-        } catch (err) {
-            console.error(err);
-        }
-    };
+  const likePost = async () => {
+    try {
+      const { data } = await addLike({ variables: { postId } });
 
-    return (
-        <div className='likes-container'>
-            <p><FontAwesomeIcon icon={ faHeart } size="lg" style={{color : "var(--brown"}} onClick={likePost} /> {likes}</p>
-        </div>
-    );
+      if (data && data.likePost && data.likePost.likes) {
+        setLikes(data.likePost.likes);
+        setLiked(true);
+      }
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  return (
+    <div className="likes-container">
+      <p>
+        {likes}{" "}
+        <FontAwesomeIcon
+          icon={faHeart}
+          size="lg"
+          style={liked ? { color: "FF0000" } : { color: "var(--brown)" }}
+          onClick={liked ? null : likePost}
+        />
+      </p>
+    </div>
+  );
 };
-
+}
 export default LikeButton;
