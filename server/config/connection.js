be73acc699669eda00d3be4d3ae28d92eeaa12f2
Alejandro <>
2024-05-26 22:58:15 -0500

    In progress; changing configurations to adapt towards a Netlify/Vercel Full Stack deployment versus reliance on Full Stack Heroku deployment to avoid payment fees

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+// MONGODB_URI - When connecting to MongoDB, search for the env variable value.
+// OR, search for the env file's value to connect.
+// Otherwise, connect to the local MongoDB Compass
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/hobby-house');
+
 
 module.exports = mongoose.connection;
